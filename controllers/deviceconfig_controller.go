1740b0e10f03747af84b3097c33aaaca420dd22a
Fabien Dupont <fdupont@redhat.com>
2022-08-22 17:52:59 +0200

    Fix permissions for node metrics
    Signed-off-by: Fabien Dupont <fdupont@redhat.com>

--- controllers/deviceconfig_controller.go
+++ controllers/deviceconfig_controller.go
@@ -81,7 +81,9 @@ func NewReconciler(
 //+kubebuilder:rbac:groups=habana.ai,resources=deviceconfigs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=habana.ai,resources=deviceconfigs/finalizers,verbs=update
 //+kubebuilder:rbac:groups="kmm.sigs.k8s.io",resources=modules,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="apps",resources=daemonsets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=nodes,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
