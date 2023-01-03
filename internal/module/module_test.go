6d01871c65dee670fc6338bf34322ae60fda80b4
Michail Resvanis <mresvani@redhat.com>
2023-01-03 15:38:46 +0100

    Adjust KMM Module firmware path
    This change removes the additional directory, i.e. `/habanalabs` from
    the KMM Module firmware path, because of the respective change on the
    KMM firmware loading implementation. The latter now skips the last
    directory of the defined firmware path.
    
    - https://github.com/kubernetes-sigs/kernel-module-management/pull/213

--- internal/module/module_test.go
+++ internal/module/module_test.go
@@ -209,11 +209,7 @@ var _ = Describe("ModuleReconciler", func() {
 
 					Expect(m.Spec.ModuleLoader.Container.Modprobe).ToNot(BeNil())
 					Expect(m.Spec.ModuleLoader.Container.Modprobe.ModuleName).To(Equal("habanalabs"))
-					Expect(m.Spec.ModuleLoader.Container.Modprobe.FirmwarePath).To(Equal("/opt/lib/firmware/habanalabs"))
-					//					modprobeParameters := []string{
-					//						"fw_path_para=/var/lib/firmware",
-					//					}
-					//					Expect(m.Spec.ModuleLoader.Container.Modprobe.Parameters).To(Equal(modprobeParameters))
+					Expect(m.Spec.ModuleLoader.Container.Modprobe.FirmwarePath).To(Equal("/opt/lib/firmware"))
 
 					Expect(m.Spec.ModuleLoader.ServiceAccountName).To(Equal(driverServiceAccount))
 				})
